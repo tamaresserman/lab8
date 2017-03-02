109b905ed49fcd983833871bf9946983238da3c4
tamaresserman <tamar.esserman@gmail.com>
2017-03-02 10:19:57 -0800

    m

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,6 +1,6 @@
 function initMap() {
   // Create center marker at UCSD
-  var ucsd_ltlng = {lat:32.884989, lng:-117.241298};
+  var ucsd_ltlng = {lat:32.880, lng:-117.236};
 
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
